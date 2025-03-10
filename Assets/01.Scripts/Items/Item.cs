0729b1aab24ec32884949997c88d0688e6f7219c
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-03-10 17:29:57 +0900

    [20250310] 타일 상호작용

---
+++ Assets/01.Scripts/Items/Item.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace JMT.Planets.Tile.Items
+{
+    public enum NaturalItemType
+    {
+        LiquidFuel,
+        OrganicMatter,
+        SpaceDust,
+        FlameIron,
+        IceIron,
+        Techron,
+    }
+
+    public enum ItemType
+    {
+        LiquidFuel, // 액체연료
+        OrganicMatter, // 유기물
+        SpaceDust, // 우주먼지
+        FlameIron, // 플레임 아이언
+        IceIron, // 아이스 아이언
+        Techron, // 테크론
+        DustBundle, // 먼지 뭉치
+        DustBoard, // 먼지 판자
+        DustSteelPlate, // 먼지 강판
+        FuelConcentrate, // 연료원액
+        ImpureWater, // 불순한 물
+        PureWater, // 정수한 물
+        SpaceBrick, // 우주 벽돌
+        Cloth, // 천
+        CulturePaper, // 배양지
+        Seed, // 씨앗
+        Fuel, // 연료
+        Ice, // 얼음
+    }
+}
---
+++ Assets/01.Scripts/Planet/Tile/TileManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace JMT.Planets.Tile
+{
+    public class TileManager : MonoBehaviour
+    {
+        // 대충 자원 모델링 프리팹 목록 넣어두고
+        // 건물 모델링 프리팹 목록 넣어두고
+        // 또머하징
+    }
+}
