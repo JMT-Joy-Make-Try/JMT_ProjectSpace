15a4391261c933b383d0de68b78ea5d54b7207c7
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-03-11 17:27:03 +0900

    [20250311] UGUI로 변경

--- Assets/01.Scripts/UI/TileUI/ItemUI.cs
+++ Assets/01.Scripts/UI/TileUI/ItemUI.cs
@@ -5,12 +5,6 @@ namespace JMT.UISystem
 {
     public class ItemUI : PanelUI
     {
-        private void OnEnable()
-        {
-            Button mineButton = root.Q<Button>("MineBtn");
-            mineButton.RegisterCallback<ClickEvent>(HandleMineButton);
-        }
-
         private void HandleMineButton(ClickEvent evt)
         {
             Debug.Log("Click Upgrade Button");
