4fc6261abf3820e97ee626253c401557ae19798a
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-03-13 17:35:08 +0900

    [20250313] 카메라 수정

--- Assets/01.Scripts/Camera/CameraInput.cs
+++ Assets/01.Scripts/Camera/CameraInput.cs
@@ -7,17 +7,13 @@ namespace JMT.CameraSystem
 {
     public class CameraInput : MonoBehaviour
     {
-        [SerializeField] private float camSpeed = 4f;
-        [SerializeField] private float rotateSpeed = 4f;
         [SerializeField] private CameraInputSO inputSO;
-        [SerializeField] private Transform camParentTrm;
-        private Transform camTransform;
-        private Coroutine zoomCoroutine;
-        private Coroutine rotateCoroutine;
+        [SerializeField] private float camSpeed = 4f;
+        [SerializeField] private float moveSpeed = 4f;
+        private Coroutine zoomCoroutine, moveCoroutine;
 
         private void Awake()
         {
-            camTransform = camParentTrm.GetChild(0);
             inputSO.OnZoomStartEvent += HandleZoomStartEvent;
             inputSO.OnZoomEndEvent += HandleZoomEndEvent;
             inputSO.OnRotateStartEvent += HandleRotateStartEvent;
@@ -41,13 +37,9 @@ namespace JMT.CameraSystem
             {
                 float distance = Vector2.Distance(inputSO.GetPrimaryPosition(), inputSO.GetSecondaryPosition());
                 float deltaDistance = distance - prevDistance;
-                Debug.Log(deltaDistance);
 
-                Vector3 targetPos = camTransform.localPosition;
-                targetPos.y -= deltaDistance * camSpeed;
-                targetPos.y = Mathf.Clamp(targetPos.z, 110f, 400f);
 
-                camParentTrm.localPosition = new Vector3(camParentTrm.localPosition.x, Mathf.Lerp(camParentTrm.localPosition.y, targetPos.y, Time.deltaTime * camSpeed));
+                Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - deltaDistance * camSpeed, 50f, 1000f);
 
                 prevDistance = distance;
                 yield return null;
@@ -55,32 +47,31 @@ namespace JMT.CameraSystem
         }
         private void HandleRotateStartEvent()
         {
-            rotateCoroutine = StartCoroutine(MoveCoroutine());
+            moveCoroutine = StartCoroutine(MoveCoroutine());
         }
 
         private void HandleRotateEndEvent()
         {
-            StopCoroutine(rotateCoroutine);
+            StopCoroutine(moveCoroutine);
         }
 
         private IEnumerator MoveCoroutine()
         {
-            float prevX = inputSO.GetPrimaryPosition().x;
-            float prevY = inputSO.GetPrimaryPosition().y;
-            Vector3 curPos = camTransform.localPosition;
+             float prevX = inputSO.GetPrimaryPosition().x;
+             float prevY = inputSO.GetPrimaryPosition().y;
+             Vector3 curPos = Camera.main.transform.localPosition;
 
-            while (true)
-            {
-                float deltaX = inputSO.GetPrimaryPosition().x - prevX;
-                float deltaY = inputSO.GetPrimaryPosition().y - prevY;
+             while (true)
+             {
+                 float deltaX = inputSO.GetPrimaryPosition().x - prevX;
+                 float deltaY = inputSO.GetPrimaryPosition().y - prevY;
 
-                camTransform.localPosition += new Vector3(-(deltaX * rotateSpeed), 0, -(deltaY * rotateSpeed));
+                 Camera.main.transform.localPosition += new Vector3(-(deltaX * moveSpeed), -(deltaY * moveSpeed));
 
-                prevX = inputSO.GetPrimaryPosition().x;
-                prevY = inputSO.GetPrimaryPosition().y;
-                yield return null;
-            }
-            
+                 prevX = inputSO.GetPrimaryPosition().x;
+                 prevY = inputSO.GetPrimaryPosition().y;
+                 yield return null;
+             }
         }
     }
 }
