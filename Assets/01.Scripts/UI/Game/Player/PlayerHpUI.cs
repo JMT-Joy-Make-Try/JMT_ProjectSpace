f309bccc7a351e82669eb907d9b63905a97d24af
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-03-26 12:59:09 +0900

    [20250326] HP 추가중

---
+++ Assets/01.Scripts/UI/Game/Player/PlayerHpUI.cs
@@ -0,0 +1,16 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JMT.UISystem
+{
+    public class PlayerHpUI : MonoBehaviour
+    {
+        [SerializeField] private Image fill;
+
+        public void SetHpBar(int currentHp, int maxHp)
+        {
+            fill.DOFillAmount(currentHp / (float)maxHp, 0.2f);
+        }
+    }
+}
