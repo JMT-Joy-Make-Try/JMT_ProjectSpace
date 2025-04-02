e5163cad990cf8892697d7a3533d198c8be08e08
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-04-02 20:02:38 +0900

    [20250402] 약간데이터수정

--- Assets/01.Scripts/Building/Data/ItemBuildingData.cs
+++ Assets/01.Scripts/Building/Data/ItemBuildingData.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace JMT.Building
 {
     public class ItemBuildingData : BaseData
     {
-        
+        [SerializeField] private List<CreateItemSO> makeItemList = new();
+
+        public List<CreateItemSO> MakeItemList => makeItemList;
     }
 }
--- Assets/01.Scripts/Items/CreateItemSO.cs
+++ Assets/01.Scripts/Items/CreateItemSO.cs
@@ -2,7 +2,7 @@ using AYellowpaper.SerializedCollections;
 using JMT.Planets.Tile.Items;
 using UnityEngine;
 
-namespace JMT.UISystem
+namespace JMT
 {
     [CreateAssetMenu(fileName = "CreateItemSO", menuName = "SO/Data/CreateItemSO")]
     public class CreateItemSO : ScriptableObject
