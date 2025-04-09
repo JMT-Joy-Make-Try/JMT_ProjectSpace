6c9b29119402ce7efae82569cb50819ee5427a27
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-04-09 10:00:20 +0900

    [20250408] 아이템 획득 표시

--- Assets/01.Scripts/Inventory/InventoryManager.cs
+++ Assets/01.Scripts/Inventory/InventoryManager.cs
@@ -1,5 +1,6 @@
 using AYellowpaper.SerializedCollections;
 using JMT.Planets.Tile.Items;
+using JMT.UISystem;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -16,9 +17,10 @@ namespace JMT.Planets.Tile
             if(!itemDictionary.ContainsKey(item))
                 itemDictionary.Add(item, increaseCount);
             else itemDictionary[item] += increaseCount;
+            UIManager.Instance.GetUI.GetItem(item, increaseCount);
         }
 
-        public void AddItem(ItemType item, int increaseCount)
+        /*public void AddItem(ItemType item, int increaseCount)
         {
             var itemSO = itemDictionary.FirstOrDefault(s => ((ItemSO)s.Key).ItemType == item).Key;
             if (itemSO == null)
@@ -29,7 +31,7 @@ namespace JMT.Planets.Tile
             if (!itemDictionary.ContainsKey(itemSO))
                 itemDictionary.Add(itemSO, increaseCount);
             else itemDictionary[itemSO] += increaseCount;
-        }
+        }*/
         
         public void RemoveItem(ItemSO item, int decreaseCount)
         {
