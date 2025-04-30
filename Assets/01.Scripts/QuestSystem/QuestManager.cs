1aab7822a078509c85c5ad15eb34b233d51e5a5f
LEE SANG GYU <tkdrb062@gmail.com>
2025-04-30 19:15:11 +0900

    feat: Quest and Lodging

---
+++ Assets/01.Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace JMT.QuestSystem
+{
+    public class QuestManager : MonoBehaviour
+    {
+        [SerializeField] private QuestListSO _questListSO;
+
+        private QuestSO _currentQuestSO;
+        private void Start()
+        {
+            _questListSO = _questListSO.Clone() as QuestListSO;
+        }
+
+        // _questListSO를 사용해서 퀘스트를 진행
+        // _questListSO의 List 순서대로 진행된다.
+        // 퀘스트를 진행할 때마다 _currentQuestSO에 현재 퀘스트를 저장한다.
+        // 퀘스트를 완료하면 다음 퀘스트로 넘어간다.
+    }
+}
