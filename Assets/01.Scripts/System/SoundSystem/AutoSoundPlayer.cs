5d9e69d11813b09552503646dc005ea8c92a8b6e
LEE SANG GYU <tkdrb062@gmail.com>
2025-05-20 23:08:20 +0900

    feat: Auto Sound Player

---
+++ Assets/01.Scripts/System/SoundSystem/AutoSoundPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace JMT.Sound
+{
+    public class AutoSoundPlayer : MonoBehaviour
+    {
+        private SoundPlayer _soundPlayer;
+        [SerializeField] private string soundKey;
+        [SerializeField] private SoundType soundType = SoundType.SFX;
+
+        private void Awake()
+        {
+            _soundPlayer = GetComponent<SoundPlayer>();
+            if (_soundPlayer == null)
+            {
+                Debug.LogError("SoundPlayer component not found on this GameObject.");
+            }
+        }
+        
+        private void Start()
+        {
+            if (_soundPlayer != null)
+            {
+                _soundPlayer.PlaySound(soundKey, soundType);
+            }
+            else
+            {
+                Debug.LogError("SoundPlayer component is not initialized.");
+            }
+        }
+    }
+}
