30335361afa3e61a23dc6290ccbf4a75e4943578
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-05-22 12:38:03 +0900

    [20250522] 퀘스트 챕터2까지 완료

--- Assets/01.Scripts/QuestSystem/Quest/Chapter02/GettingQuest.cs
+++ Assets/01.Scripts/QuestSystem/Quest/Chapter02/GettingQuest.cs
@@ -1,23 +1,40 @@
 using JMT.Planets.Tile;
+using System;
 
 namespace JMT.QuestSystem
 {
     public class GettingQuest : QuestBase
     {
+        private Action<TileInteraction>[] handlers;
+
         private void Start()
         {
-            tiles[0].OnChangeInteraction += HandleChangeInteraction;
+            handlers = new Action<TileInteraction>[tiles.Count];
+
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                int tileIndex = i;
+
+                // 이름 없는 함수(람다)로 쓰게 되면 OnDestroy에서 문제가 발생할 수도 있음.
+                // 따라서 수행할 함수를 변수로 저장해서 핸들러를 등록함.
+                handlers[tileIndex] = (interaction) => HandleChangeInteraction(interaction, tileIndex);
+                tiles[tileIndex].OnChangeInteraction += handlers[tileIndex];
+            }
         }
 
         private void OnDestroy()
         {
-            tiles[0].OnChangeInteraction -= HandleChangeInteraction;
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                int tileIndex = i;
+                tiles[tileIndex].OnChangeInteraction -= handlers[tileIndex];
+            }
         }
 
-        private void HandleChangeInteraction(TileInteraction interaction)
+        private void HandleChangeInteraction(TileInteraction interaction, int index)
         {
             if(interaction.GetType() == typeof(NoneInteraction))
-                RunQuest(0);
+                RunQuest(index);
         }
     }
 }
