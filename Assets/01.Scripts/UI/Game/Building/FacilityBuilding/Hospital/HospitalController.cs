26a83eedebc7f3044fc8f81cb13fff01631e9186
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-06-05 10:06:20 +0900

    [20250605] 치료소 UI 완료...?

--- Assets/01.Scripts/UI/Game/Building/FacilityBuilding/Hospital/HospitalController.cs
+++ Assets/01.Scripts/UI/Game/Building/FacilityBuilding/Hospital/HospitalController.cs
@@ -1,14 +1,31 @@
+using JMT.Agent;
+using JMT.Agent.NPC;
+using JMT.Building.Component;
+using JMT.Core.Manager;
+using JMT.Planets.Tile;
 using JMT.UISystem.DayTime;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace JMT.UISystem.Hospital
 {
     public class HospitalController : MonoBehaviour
     {
         [SerializeField] private HospitalView view;
+        [Header("Worker View")]
         [SerializeField] private HospitalWorkerView workerView;
+        [SerializeField] private NPCContentUI workerContent;
+        [SerializeField] private HospitalWorkerSelectView selectView;
+
+        [Header("Patient View")]
         [SerializeField] private HospitalPatientView patientView;
+        [SerializeField] private HospitalPatientStatView statView;
+
+        [Header("Upgrade View")]
         [SerializeField] private HospitalUpgradeView upgradeView;
+
 
         private PanelUI currentPanel;
 
@@ -18,7 +35,15 @@ namespace JMT.UISystem.Hospital
             view.OnPatientButtonEvent += HandlePatientEvent;
             view.OnUpgradeButtonEvent += HandleUpgradeEvent;
             view.OnExitButtonEvent += ClosePanel;
+
+            workerContent.OnAddEvent += HandleAddEvent;
+            workerContent.OnQuitEvent += HandleQuitEvent;
+
+            selectView.OnHireEvent += HandleHireEvent;
+
+            patientView.OnClickPatientEvent += HandleClickPatientEvent;
         }
+
 
         private void OnDestroy()
         {
@@ -59,10 +84,44 @@ namespace JMT.UISystem.Hospital
             SetCurrentPanel(upgradeView);
         }
 
+        private void HandleAddEvent()
+        {
+            // 고용하기 버튼
+            selectView.OpenUI();
+            selectView.SetWorkerContent(AgentManager.Instance.UnemployedAgents);
+        }
+
+        private void HandleHireEvent(NPCAgent agent)
+        {
+            if (agent == null) return;
+
+            Debug.Log("여기 수정해주세요.");
+            var lodgingBuilding = BuildingManager.Instance.LodgingBuildings[Random.Range(0, BuildingManager.Instance.LodgingBuildings.Count)];
+            if (lodgingBuilding == null) return;
+            var spawnPos = lodgingBuilding.transform.position;
+
+            AgentManager.Instance.SpawnNpc(spawnPos, Quaternion.identity);
+            TileManager.Instance.CurrentTile.CurrentBuilding.GetBuildingComponent<BuildingNPC>().AddNpc(agent);
+            selectView.CloseUI();
+        }
+
+        private void HandleQuitEvent()
+        {
+            workerContent.ActiveLockArea(true);
+            TileManager.Instance.CurrentTile.CurrentBuilding.GetBuildingComponent<BuildingNPC>().RemoveNpc();
+        }
+
+        private void HandleClickPatientEvent(NPCAgent agent)
+        {
+            statView.OpenUI();
+            statView.SetStatPanel(agent);
+        }
+
         public void SetCurrentPanel(PanelUI panel)
         {
             if (currentPanel == panel) return;
-            //sideView.CloseUI();
+            selectView.CloseUI();
+            statView.CloseUI();
             currentPanel?.CloseUI();
             currentPanel = panel;
             currentPanel?.OpenUI();
