3986ed81709c3fc7b78145ac9a00b9123a164fc1
LEE SANG GYU <tkdrb062@gmail.com>
2025-06-09 17:05:15 +0900

    refactor: Progress Interaction

--- Assets/01.Scripts/Building/BuildingComponent/BuildingVisual.cs
+++ Assets/01.Scripts/Building/BuildingComponent/BuildingVisual.cs
@@ -11,6 +11,8 @@ namespace JMT.Building.Component
         [SerializeField] private List<MeshRenderer> rendererList;
         [SerializeField] private List<GameObject> buildingLevelObjects;
         
+        private GameObject _currentLevelObject;
+        
         public BuildingBase Building { get; private set; }
         public Material VisualMat => visualMat;
         public List<MeshFilter> MeshFilters => rendererList.ConvertAll(renderer => renderer.GetComponent<MeshFilter>());
@@ -25,6 +27,36 @@ namespace JMT.Building.Component
                     rendererList[i].material = Instantiate(rendererList[i].material);
                 }
             }
+
+            _currentLevelObject = buildingLevelObjects[0];
+        }
+
+        private void OnDestroy()
+        {
+            Building.GetBuildingComponent<BuildingLevel>().OnLevelChanged -= OnBuildingLevelChanged;
+        }
+
+        private void OnBuildingLevelChanged(int level)
+        {
+            if (buildingLevelObjects == null || buildingLevelObjects.Count == 0)
+            {
+                Debug.LogWarning("Building level objects are not set or empty.");
+                return;
+            }
+            if (_currentLevelObject == null)
+            {
+                Debug.LogWarning("Current level object is null.");
+                _currentLevelObject = buildingLevelObjects[0];
+            }
+            
+            _currentLevelObject.SetActive(false);
+            _currentLevelObject = buildingLevelObjects[level];
+            if (_currentLevelObject == null)
+            {
+                Debug.LogWarning($"Building level object for level {level} is null.");
+                return;
+            }
+            _currentLevelObject.SetActive(true);
         }
 
         public void SetFloatProperty(string propertyName, float value, bool isAllRendererChange = false)
@@ -71,6 +103,7 @@ namespace JMT.Building.Component
         public void Init(BuildingBase building)
         {
             Building = building;
+            Building.GetBuildingComponent<BuildingLevel>().OnLevelChanged += OnBuildingLevelChanged;
         }
     }
 }
