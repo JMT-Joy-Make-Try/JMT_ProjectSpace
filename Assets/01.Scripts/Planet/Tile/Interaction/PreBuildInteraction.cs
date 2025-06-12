e4538875154efa87292aa850866daa01ee41be8a
LEE SANG GYU <tkdrb062@gmail.com>
2025-06-12 23:37:18 +0900

    fix: PreBuild Bug

--- Assets/01.Scripts/Planet/Tile/Interaction/PreBuildInteraction.cs
+++ Assets/01.Scripts/Planet/Tile/Interaction/PreBuildInteraction.cs
@@ -28,12 +28,12 @@ namespace JMT.Planets.Tile
                 return;
             }
 
-            _requiredItems[inventory.PlayerInventoryData.item] -= inventory.PlayerInventoryData.count;
-            inventory.RemoveItem();
+            _requiredItems[inventory.PlayerInventoryData.item] -= 1;
             if (_requiredItems[inventory.PlayerInventoryData.item] <= 0)
             {
                 _requiredItems.Remove(inventory.PlayerInventoryData.item);
             }
+            inventory.RemoveItem();
             _isCompleteReceive = _requiredItems.Count <= 0;
             
             if (_isCompleteReceive && !_isBuildComplete)
