39c89b1e5b0c77e7dc10d489c866e8a6b4deda65
LEE SANG GYU <tkdrb062@gmail.com>
2025-06-18 19:08:07 +0900

    feat: NPC Stat

---
+++ Assets/01.Scripts/Agent/NPC/Stat/NPCStatType.cs
@@ -0,0 +1,16 @@
+﻿namespace JMT.Agent
+{
+    public enum NPCStatType
+    {
+        Dexterity, // 손재주
+        Stamina, // 체력
+        LungCapacity, // 폐활량
+        Satisfaction, // 만족도
+    }
+
+    public enum NPCStatModifierType
+    {
+        Additive,
+        Multiplicative,
+    }
+}
