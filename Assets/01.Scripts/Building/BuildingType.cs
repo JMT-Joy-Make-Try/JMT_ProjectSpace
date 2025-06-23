19566a6c777e31a3078d6ad023d18e63654c06a5
LEE SANG GYU <tkdrb062@gmail.com>
2025-06-23 19:35:38 +0900

    feat: BuildingType

--- Assets/01.Scripts/Building/BuildingType.cs
+++ Assets/01.Scripts/Building/BuildingType.cs
@@ -2,6 +2,15 @@
 {
     public enum BuildingType
     {
-        Null,
+        BaseBuilding, // 기지
+        LaboratoryBuilding, // 연구소
+        LodgingBuilding, // 숙소
+        OxygenBuilding, // 산소 공급기
+        HospitalBuilding, // 치료소
+        DustCollectorBuilding, // 먼지 채집소
+        DustPressingBuilding, // 먼지 압축소
+        PlantBuilding, // 유기물 채집소
+        LiquidFuelBuilding, // 액체 연료 수집소
+        RocketLaunchBuilding, // 로켓 발사대
     }
 }
