f3095906e36655fa244007100c1f19bf186478ea
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-06-24 14:15:30 +0900

    [20250624] 필요아이템 비활성화

--- Assets/01.Scripts/QuestSystem/Quest/Chapter01/GuestQuest.cs
+++ Assets/01.Scripts/QuestSystem/Quest/Chapter01/GuestQuest.cs
@@ -1,9 +1,14 @@
+using JMT.Agent;
 using UnityEngine;
 
 namespace JMT.QuestSystem
 {
     public class GuestQuest : QuestBase
     {
-        
+        public override void Enable()
+        {
+            base.Enable();
+            AgentManager.Instance.SpawnTrader(Tiles[0].transform.position, Quaternion.identity);
+        }
     }
 }
