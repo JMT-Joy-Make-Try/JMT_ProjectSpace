fa327e0f7d222422bfb495d64865a9c394b7c335
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-06-30 19:19:42 +0900

    [20250630] UI 업데이트

--- Assets/01.Scripts/QuestSystem/QuestBase.cs
+++ Assets/01.Scripts/QuestSystem/QuestBase.cs
@@ -128,7 +128,6 @@ public class QuestBase : MonoBehaviour, IQuestTarget
 
     private void QuestCountEvent()
     {
-        Debug.Log($"{CompleteCount}/{QuestPing.Count}");
         QuestManager.Instance.OnQuestCountEvent?.Invoke($"{CompleteCount}/{QuestPing.Count}");
     }
 }
